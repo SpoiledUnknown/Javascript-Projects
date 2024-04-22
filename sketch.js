5ec6ddf5b13fac7a6b52464f765c1a22984e460c
Spoiled Unknown <shanjusamsong254@gmail.com>
2024-04-22 21:04:26 +0530

    initial commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var canvas,backgroundimage;
+var gameState = 1;
+var playerCount;
+var database;
+var form,player,game;
+var allPlayers;
+var distance = 0;
+
+function setup ()
+{
+canvas=createCanvas(400,400);
+database=firebase.database();
+game = new Game();
+game.getState();
+game.start();
+game.update(gameState);
+}
+
+function draw ()
+{
+if(playerCount===4)
+{
+    game.update(2);
+}
+if(gameState === 2)
+{
+    clear();
+    game.play();
+}
+}
