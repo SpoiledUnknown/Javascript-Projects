b6a18f5a3bb131022ea8a71dd61752fa8f530927
Spoiled Unknown <58999771+SpoiledUnknown@users.noreply.github.com>
2024-08-08 11:45:27 +0530

    minor improvements.

--- sketch.js
+++ sketch.js
@@ -1,30 +1,26 @@
-var canvas,backgroundimage;
+var canvas, backgroundimage;
 var gameState = 1;
 var playerCount;
 var database;
-var form,player,game;
+var form, player, game;
 var allPlayers;
 var distance = 0;
 
-function setup ()
-{
-canvas=createCanvas(400,400);
-database=firebase.database();
-game = new Game();
-game.getState();
-game.start();
-game.update(gameState);
+function setup() {
+  canvas = createCanvas(400, 400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  game.update(gameState);
 }
 
-function draw ()
-{
-if(playerCount===4)
-{
+function draw() {
+  if (playerCount === 4) {
     game.update(2);
-}
-if(gameState === 2)
-{
+  }
+  if (gameState === 2) {
     clear();
     game.play();
-}
+  }
 }
